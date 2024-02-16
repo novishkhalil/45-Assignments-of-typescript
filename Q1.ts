75029779277b1de0457741989491bad8d531f44f
novishkhalil <159153731+novishkhalil@users.noreply.github.com>
2024-02-16 18:20:11 +0500

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,7 @@
+// 1. Install [Node.js](https://nodejs.org/en/),
+// [TypeScript](https://www.typescriptlang.org/download)  and
+// [VS Code](https://code.visualstudio.com/) on your computer.
+
+// npm i -g typescript for (typescript install)
+// tsc --init for (tsconfig.json)
+
